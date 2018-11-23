be219f00eacabaa143b0a459a595a23938edd11a
mg182028 <41511355+mg182028@users.noreply.github.com>
2018-11-23 17:49:50 -0500

    Rev2 for 11/23/2018
    Adding a few more statements to practice making changes on a branch.

--- Hello.py
+++ Hello.py
@@ -1,10 +1,23 @@
 import time
 print("Hello, World!")
 print('Test')
-print("Hello, Dad!", end='')
+print("Hello, Dad!")  # , end='')
 time.sleep(2)
 print('Boo!')
 print('Aidan is cool')
-# Added 11/23/2018 by Dad in dad_hello branch
+# Added 11/23/2018_rev1 by Dad in dad_hello branch
 print('Aidan and Andrew are still cool.')
+# Added 11/23/2018_rev2 by Dad in dad_hello branch
+person = "Aidan"
+person = "Andrew"
+#Person = "Dad"
+
+if person == "Aidan":
+    print('Person is ', person)
+elif person == "Andrew":
+    print('Person is ', person)
+else:
+    print('I guess were stuck with Dad')
+
+
 
