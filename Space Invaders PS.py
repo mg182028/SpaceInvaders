7ec9967105d186fdab11c19cf8bdc30d905156dd
mg182028 <41511355+mg182028@users.noreply.github.com>
2018-11-23 16:17:14 -0500

    Working Basic Game
    The game works, but is really bare bones

--- Space Invaders PS.py
+++ Space Invaders PS.py
@@ -12,10 +12,6 @@ canvas.pack()
 canvas.configure(background='pink')
 tk.update()
 
-
-
-
-
 class PlayerShip:
         
     def __init__(self, canvas, color):
@@ -30,27 +26,25 @@ class PlayerShip:
         self.canvas.bind_all('<KeyPress-d>', self.turn_right)
         
     def turn_left(self, evt):
-        self.x = -10
+        pos = self.canvas.coords(self.id)
+        if pos[0] > 0:
+            self.x = -10
         
     def turn_right(self, evt):
-        self.x = 10
-        
+        pos = self.canvas.coords(self.id)
+        if pos[2] <= (self.canvas_width-10):
+            self.x = 10
+
     def draw(self):
         self.canvas.move(self.id, self.x, 0)
-        pos = self.canvas.coords(self.id)
-        if pos[0] <= 0:
-            self.x = 10
-        elif pos[2] >= self.canvas_width:
-            self.x = -20
-        elif self.canvas.bind_all('<KeyPress-Left>', self.turn_left) or self.canvas.bind_all('<KeyPress-a>', self.turn_left) :
+        if self.canvas.bind_all('<KeyPress-Left>', self.turn_left) or self.canvas.bind_all('<KeyPress-a>', self.turn_left) :
             self.x = 0
-        
-
 
 PlayerShip = PlayerShip(canvas, 'purple')
 
 while 1:
     PlayerShip.draw()
     tk.update_idletasks()
+    time.sleep(0.001)
     tk.update()
-    time.sleep(0.01)
+
